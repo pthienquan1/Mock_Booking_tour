ce3b8834645a9499d1adb7376ad7bb331068c207
pthienquan1 <quan01011997123@gmail.com>
2023-08-29 20:25:22 +0700

    init prj

---
+++ src/main/java/com/vti/booking_tour/BookingTourApplication.java
@@ -0,0 +1,13 @@
+package com.vti.booking_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingTourApplication.class, args);
+	}
+
+}
